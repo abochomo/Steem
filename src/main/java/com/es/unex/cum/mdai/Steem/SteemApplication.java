1c5c6f9b5bff967a83a98ea3f5d0e69d79c7052c
abochomo <xd3vi0us@gmail.com>
2025-10-23 17:49:14 +0200

    Creación del proyecto

---
+++ src/main/java/com/es/unex/cum/mdai/Steem/SteemApplication.java
@@ -0,0 +1,13 @@
+package com.es.unex.cum.mdai.Steem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/es/unex/cum/mdai/Steem/SteemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.unex.cum.mdai.Steem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
