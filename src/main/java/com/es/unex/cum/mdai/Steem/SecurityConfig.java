9695a0930a8c85b3ef95834645b156c5e6cddeff
Alejandro <abochomo@alumnos.unex.es>
2025-11-30 11:33:43 +0100

    Comentadas las funciones de SpringSecurity de manera temporal

--- src/main/java/com/es/unex/cum/mdai/Steem/SecurityConfig.java
+++ src/main/java/com/es/unex/cum/mdai/Steem/SecurityConfig.java
@@ -1,5 +1,5 @@
 package com.es.unex.cum.mdai.Steem;
-
+/*
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -46,3 +46,4 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(admin);
     }
 }
+ */
